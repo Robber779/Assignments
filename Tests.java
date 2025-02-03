8f0b58a1fe8a8d3ba0ee9360434a74476c181860
Robber779 <2jrdavis779@gmail.com>
2025-02-02 22:47:34 -0500

    Add files via upload

---
+++ Tests.java
@@ -0,0 +1,12 @@
+public class Tests{
+   public static void main(String[] args){
+      double t1 = 97.3;
+      double t2 = 84.7;
+      double t3 = 62.1;
+      System.out.println("Test score 1: "+t1);
+      System.out.println("Test score 2: "+t2);
+      System.out.println("Test score 3: "+t3);
+      double avg = (t1+t2+t3)/3;
+      System.out.println("The average of 3 test scores is: "+avg);
+   }
+}
