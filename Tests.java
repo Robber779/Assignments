0b6aaef87385bed65cac1cfc013004768c73540d
Robber779 <2jrdavis779@gmail.com>
2025-02-16 23:18:55 -0500

    Add files via upload

--- Tests.java
+++ Tests.java
@@ -1,12 +1,61 @@
+import java.util.Scanner;
 public class Tests{
+
+   private double ave;
+   private int score, count;
+   
+   public Tests(){
+      ave = 0;
+      count = 0;
+      this.setScore(0);
+      
+   }
+   
+   public double getAve(){
+      return ave;
+   }
+   
+   public void getAverage(){
+      Scanner scan = new Scanner(System.in);
+      double sum = 0;
+      int count2 = -1;
+      while(getScore() != -1){
+         sum+= getScore();
+         count2++;
+         System.out.println("To calculate the average, please enter a number below. Enter -1 to stop."+score);
+         setScore(scan.nextInt());
+      }
+      
+      if(count2!=-1){ ave = sum/count2; }
+      count = count2;
+   }
+   
+   public int getScore(){//mostly confused on what this is used for
+      return score;
+   }
+   
+   public int getCount(){
+      return count;
+   }
+   
+   public void setScore(int newScore){//mostly confused on what this is used for
+      score = newScore;
+      
+   }
+   
+   public String toString(){
+      String output;
+      if(count == 0){
+         return "The average of the 0 scores entered is NaN";
+         //return output;
+      }
+      return String.format("The average of the "+getCount()+" scores entered is %.2f.", getAve());//String.format and %.(whatever number)f limits the amount of decimal places
+   }
+
    public static void main(String[] args){
-      double t1 = 97.3;
-      double t2 = 84.7;
-      double t3 = 62.1;
-      System.out.println("Test score 1: "+t1);
-      System.out.println("Test score 2: "+t2);
-      System.out.println("Test score 3: "+t3);
-      double avg = (t1+t2+t3)/3;
-      System.out.println("The average of 3 test scores is: "+avg);
+      Tests test1 = new Tests();
+      test1.getAverage();
+      System.out.println(test1);
+      
    }
 }
